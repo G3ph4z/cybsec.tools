76e13a270a6fac4ab0e4e4672fb57deb529bde7e
G3ph4z <26697727+G3ph4z@users.noreply.github.com>
2023-12-17 20:31:53 +0000

    update(frontend): Add pagination to the list

--- frontend/cybsec-frontend/src/Lists.tsx
+++ frontend/cybsec-frontend/src/Lists.tsx
@@ -1,52 +1,78 @@
 import React, { useState, useEffect } from 'react';
 import './Lists.css';
 
-const apiUrl = process.env.BACKEND_API_ENDPOINT || 'http://localhost:5000/lists';
+const apiUrl = 'http://backend:5000';
 
 const Lists: React.FC = () => {
   const [toolData, setToolData] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [itemsPerPage] = useState<number>(30);
 
   useEffect(() => {
-    fetch(apiUrl)
+    setLoading(true);
+    fetch(`${apiUrl}/lists?page=${currentPage}&limit=${itemsPerPage}`)
       .then((response) => response.json())
-      .then((data) => setToolData(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
+      .then((data) => {
+        setToolData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
+  }, [currentPage, itemsPerPage]);
 
   return (
     <div className="tool-list-container">
-      <table className="tool-list-table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>URL</th>
-            <th>Mitre Category</th>
-            <th>Tags</th>
-          </tr>
-        </thead>
-        <tbody>
-          {toolData.map((tool) => (
-            <tr key={tool.id}>
-              <td>{tool.id}</td>
-              <td>{tool.name}</td>
-              <td><a href={tool.url} target="_blank" rel="noopener noreferrer">{tool.url}</a></td>
-              <td>
-                {/* Render category badges */}
-                {tool.mitre_cat.split(', ').map((category: string, index: number) => (
-                  <span key={index} className="badge category-badge">{category}</span>
-                ))}
-              </td>
-              <td>
-                {/* Render tag badges */}
-                {tool.tags.split(', ').map((tag: string, index: number) => (
-                  <span key={index} className="badge tag-badge">{tag}</span>
-                ))}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {loading ? ( 
+        <div>Loading...</div>
+      ) : (
+        <>
+          <table className="tool-list-table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>URL</th>
+                <th>Mitre Category</th>
+                <th>Tags</th>
+              </tr>
+            </thead>
+            <tbody>
+              {toolData.map((tool) => (
+                <tr key={tool.id}>
+                  <td>{tool.name}</td>
+                  <td><a href={tool.url} target="_blank" rel="noopener noreferrer">{tool.url}</a></td>
+                  <td>
+                    {/* Render category badges */}
+                    {tool.mitre_cat.split(', ').map((category: string, index: number) => (
+                      <span key={index} className="badge category-badge">{category}</span>
+                    ))}
+                  </td>
+                  <td>
+                    {/* Render tag badges */}
+                    {tool.tags.split(', ').map((tag: string, index: number) => (
+                      <span key={index} className="badge tag-badge">{tag}</span>
+                    ))}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          {/* Pagination controls */}
+          <div className="pagination">
+            <button
+              disabled={currentPage === 1}
+              onClick={() => setCurrentPage((prevPage) => prevPage - 1)}
+            >
+              Previous
+            </button>
+            <span>{currentPage}</span>
+            <button onClick={() => setCurrentPage((prevPage) => prevPage + 1)}>Next</button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
